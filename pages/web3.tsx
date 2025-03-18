01bc1fc9471e03acfc181ed7317d3a00c16b1a62
respect-778 <3987902228@qq.com>
2025-03-18 17:22:21 +0800

    Update web3.tsx

--- pages/web3.tsx
+++ pages/web3.tsx
@@ -1,8 +1,13 @@
-import { createConfig, http, useReadContract, useWriteContract } from "wagmi";
-import { mainnet, sepolia } from "wagmi/chains";
+import {
+  http,
+  useReadContract,
+  useWriteContract,
+  useWaitForTransactionReceipt,
+} from "wagmi";
 import {
   WagmiWeb3ConfigProvider,
   MetaMask,
+  Mainnet,
   Sepolia,
 } from "@ant-design/web3-wagmi";
 import {
@@ -12,45 +17,47 @@ import {
   ConnectButton,
   useAccount,
 } from "@ant-design/web3";
-import { injected } from "wagmi/connectors";
-import { Button, message } from "antd";
+import { Button, message, Flex } from "antd";
 import { parseEther } from "viem";
+import { useEffect } from "react";
 
-const config = createConfig({
-  chains: [mainnet, sepolia],
-  transports: {
-    [mainnet.id]: http('https://api.zan.top/node/v1/eth/mainnet/edab0ddddb444fec9c5a5f97496a7167'),
-    [sepolia.id]: http('https://api.zan.top/node/v1/eth/sepolia/edab0ddddb444fec9c5a5f97496a7167'),
-  },
-  connectors: [
-    injected({
-      target: "metaMask",
-    }),
-  ],
-});
+// Sepolia test contract 0x81BaD6F768947D7741c83d9EB9007e1569115703
+const CONTRACT_ADDRESS = "0xEcd0D12E21805803f70de03B72B1C162dB0898d9";
 
 const CallTest = () => {
   const { account } = useAccount();
   const result = useReadContract({
     abi: [
       {
-        type: 'function',
-        name: 'balanceOf',
-        stateMutability: 'view',
-        inputs: [{ name: 'account', type: 'address' }],
-        outputs: [{ type: 'uint256' }],
+        type: "function",
+        name: "balanceOf",
+        stateMutability: "view",
+        inputs: [{ name: "account", type: "address" }],
+        outputs: [{ type: "uint256" }],
       },
     ],
-    address: '0xEC0998ec041ef40870D33996d842252CBdc7C170',
-    functionName: 'balanceOf',
+    address: CONTRACT_ADDRESS,
+    functionName: "balanceOf",
     args: [account?.address as `0x${string}`],
   });
-  const { writeContract } = useWriteContract();
+  const { data: hash, writeContract } = useWriteContract();
+  const { isLoading: isConfirming, isSuccess: isConfirmed } =
+    useWaitForTransactionReceipt({
+      hash,
+    });
+
+  useEffect(() => {
+    if (isConfirmed) {
+      message.success("Mint Success");
+      result.refetch();
+    }
+  }, [isConfirmed]);
 
   return (
-    <div>
+    <Flex gap={12} align="center">
       {result.data?.toString()}
       <Button
+        loading={isConfirming}
         onClick={() => {
           writeContract(
             {
@@ -69,15 +76,12 @@ const CallTest = () => {
                   outputs: [],
                 },
               ],
-              address: "0xEC0998ec041ef40870D33996d842252CBdc7C170",
+              address: CONTRACT_ADDRESS,
               functionName: "mint",
               args: [BigInt(1)],
               value: parseEther("0.01"),
             },
             {
-              onSuccess: () => {
-                message.success("Mint Success");
-              },
               onError: (err) => {
                 message.error(err.message);
               },
@@ -87,16 +91,26 @@ const CallTest = () => {
       >
         mint
       </Button>
-    </div>
+    </Flex>
   );
-}
+};
 
 export default function Web3() {
   return (
     <WagmiWeb3ConfigProvider
-      config={config}
-      chains={[Sepolia]}
+      chains={[Mainnet, Sepolia]}
       wallets={[MetaMask()]}
+      eip6963={{
+        autoAddInjectedWallets: true,
+      }}
+      transports={{
+        [Mainnet.id]: http(
+          "https://api.zan.top/node/v1/eth/mainnet/7f039b4a093940a8bb5d2f76cca81e45"
+        ),
+        [Sepolia.id]: http(
+          "https://api.zan.top/node/v1/eth/sepolia/7f039b4a093940a8bb5d2f76cca81e45"
+        ),
+      }}
     >
       <Address format address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9" />
       <NFTCard
@@ -109,4 +123,4 @@ export default function Web3() {
       <CallTest />
     </WagmiWeb3ConfigProvider>
   );
-};
+}
